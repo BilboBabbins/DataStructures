883117bb9a5f2c58e95d61828a27985962299b85
BibloBabbins <ellie3na@gmail.com>
2021-08-22 12:46:24 -0700

    Started on tries. Working on add. Its a auto search type thing.Lists are very helpful

---
+++ Trie/Trie/TrieTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+   
+    class TrieNode
+    {
+        public List<TrieNode> Children;
+        public char Value;
+
+        public TrieNode(char val)
+        {
+            Value = val;
+        }
+
+    }
+
+    class TrieTree
+    {
+        public TrieNode Head;
+
+        public bool Contains(TrieNode current, char letter)
+        {
+            for (int a = 0; a < current.Children.Count; a++)
+            {
+                if (current.Children[a].Value == letter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Add(string word)
+        { 
+           
+        }
+            
+
+    }
+}
